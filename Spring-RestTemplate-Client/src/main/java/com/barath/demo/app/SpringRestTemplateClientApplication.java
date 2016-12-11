0f9d3faf76861ba66087dbe20d3e8ef188d30209
Barath Arivazhagan <barathmacdec91@gmail.com>
2016-12-11 23:10:09 +0530

    Add files via upload

---
+++ Spring-RestTemplate-Client/src/main/java/com/barath/demo/app/SpringRestTemplateClientApplication.java
@@ -0,0 +1,20 @@
+package com.barath.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringRestTemplateClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestTemplateClientApplication.class, args);
+	}
+	
+	@GetMapping("/test")
+	public String test(){
+		return "Response from REST client";
+	}
+}
