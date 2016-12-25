b955ea5dbbefece4badfe7fb215c6c38946753dc
Barath Arivazhagan <barathmacdec91@gmail.com>
2016-12-25 14:25:18 +0530

    Update Publisher.java

--- Spring-Event-Driven-Application/src/main/java/com/barath/app/Publisher.java
+++ Spring-Event-Driven-Application/src/main/java/com/barath/app/Publisher.java
@@ -1,36 +1,29 @@
 package com.barath.app;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicInteger;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import reactor.bus.Event;
 import reactor.bus.EventBus;
 
+
+@Service
 public class Publisher {
+	
+	private static final Logger logger=LoggerFactory.getLogger(Publisher.class);
 	
 	@Autowired
 	EventBus eventBus;
 
-	@Autowired
-	CountDownLatch latch;
 
-	public void publishQuotes(int numberOfQuotes) throws InterruptedException {
-		long start = System.currentTimeMillis();
-
-		AtomicInteger counter = new AtomicInteger(1);
-
-		for (int i = 0; i < numberOfQuotes; i++) {
-			eventBus.notify("quotes", Event.wrap(counter.getAndIncrement()));
-		}
-
-		latch.await();
-
-		long elapsed = System.currentTimeMillis() - start;
-
-		System.out.println("Elapsed time: " + elapsed + "ms");
-		System.out.println("Average time per quote: " + elapsed / numberOfQuotes + "ms");
+	public void publishEvent(String name) throws InterruptedException {
+			logger.info("publisher is called");		
+			eventBus.notify("name", Event.wrap(name));
 	}
+
+	
+	
 
 }
