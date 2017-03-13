fe02703d616d54e319413a10445faa267926fbf7
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-03-13 22:27:30 +0530

    Simple Spring Boot application is added

---
+++ Spring-Boot-App-Simple/src/main/java/com/barath/app/Application.java
@@ -0,0 +1,26 @@
+package com.barath.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@GetMapping("/")
+	public String home(){
+		return "hello, welcome to application";
+	}
+	
+	@GetMapping("/get/{name}")
+	public String getName(@PathVariable String name){
+		return "hello, welcome "+name;
+	}
+}
